72fef7cdec62bb83702d2e27effbb6cea89f77df
hosam mohamed <hosamelmalt@gmail.com>
2020-08-31 14:04:32 -0700

    Initial Commit

---
+++ Music App Design/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Music App Design
+//
+//  Created by Hossam on 8/31/20.
+//  Copyright © 2020 Hossam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
